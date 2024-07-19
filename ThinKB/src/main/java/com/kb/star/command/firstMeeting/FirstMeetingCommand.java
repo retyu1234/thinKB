a892736abca3ec7e756947cd6e04aa6484a915f5
SJ <suji_95@naver.com>
2024-07-19 17:34:22 +0900

    delete ideaMeeting class

--- ThinKB/src/main/java/com/kb/star/command/firstMeeting/FirstMeetingCommand.java
+++
@@ -1,9 +0,0 @@
-package com.kb.star.command.firstMeeting;
-
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.ui.Model;
-
-public interface FirstMeetingCommand {
-
-	public void execute(Model model);
-}
